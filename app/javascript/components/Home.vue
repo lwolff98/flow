444066d70a2663c8fdd3192742d92073034c2d75
lwolff98 <lucywolff89@gmail.com>
2023-03-29 16:03:53 -0400

    first commit

---
+++ app/javascript/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Welcome!</h1>
+    <button type="button">Create Flow</button>
+    <button type="button">Browse Past Flows</button>
+  </template>
+  
+  <script>
+
+  export default {
+    components: {
+    },
+    data() {
+      return {};
+    },
+  };
+  </script>
